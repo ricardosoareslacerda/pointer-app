c7ea91856ad3c1994f999a330441a6763cb72296
ricardosoareslacerda <66573448+ricardosoareslacerda@users.noreply.github.com>
2024-01-27 23:44:51 -0300

    Update dependencias.sh

--- roles/pointer-app/dependencias.sh
+++ roles/pointer-app/dependencias.sh
@@ -2,7 +2,7 @@
 sudo apt-get update 
 sudo apt-get install git ansible -y
 
-git clone git@github.com:ricardosoareslacerda/pointer-app.git
+git clone git@github.com:ricardosoareslacerda/pointer-app.git /tmp/pointer-apps/
 
 cd /tmp/pointer-apps/
 ansible-playbook pointer-playbook.yml
